f0177d2952b24a6a783783bf47a59fadc1978109
dracher <dracher@gmail.com>
2019-07-25 11:15:24 +0800

    update summary with new content

--- static/js/site.js
+++ static/js/site.js
@@ -1,7 +1,13 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $(".sidenav").sidenav()
     $(".parallax").parallax()
     $("select").formSelect()
     $(".modal").modal()
-    $('.collapsible').collapsible()
+    $(".collapsible").collapsible()
+
+    $("#summary_more").hover(function() {
+        console.log(123)
+        $("#summary_intro").show({ duration: 100, easing: "linear" })
+        $(this).hide()
+    })
 })
